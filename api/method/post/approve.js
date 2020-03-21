9ec5cdf8f5dba14816d6bbf3134282cb37311855
MrFermz <Mr.Fermz@hotmail.com>
2020-03-21 22:01:56 +0700

    add desync check

--- api/method/post/approve.js
+++ api/method/post/approve.js
@@ -7,22 +7,45 @@ async function approve(req, res, body) {
     let token       = await verifyToken(req, res)
 
     if (token) {
-        let data        = JSON.parse(body)
-        let sql         = `UPDATE leaves
-                           SET    status      = 1, 
-                                  dateApprove = '${data.dateApprove}'
-                           WHERE  leaveID     = ${data.id}`
-        db.query(sql, function (error, result) {
+        let data    = JSON.parse(body)
+        let sql     = `SELECT status FROM leaves WHERE leaveID = ${data.id}`
+        db.query(sql, async function (error, result) {
             if (error) {
                 result_failed['data']   = error
                 res.end(JSON.stringify(result_failed))
             } else {
-                res.end(JSON.stringify(result_success))
+                console.log(result[0].status)
+                if (result[0].status == 0) {
+                    let update  = await updateStatus(data)
+                    if (update.result == 'success') {
+                        res.end(JSON.stringify(result_success))
+                    }
+                } else {
+                    result_failed['data']   = 'desync'
+                    res.end(JSON.stringify(result_failed))
+                }
             }
         })
     } else {
         res.end(JSON.stringify(result_failed))
     }
+}
+
+
+function updateStatus(data) {
+    return new Promise(function (resolve, reject) {
+        let sql     = `UPDATE   leaves
+                        SET     status      = 1,
+                                dateApprove = '${data.dateApprove}'
+                        WHERE   leaveID     = ${data.id}`
+        db.query(sql, function (error, result) {
+            if (error) {
+                reject(error)
+            } else {
+                resolve(result_success)
+            }
+        })  
+    })
 }
 
 
