6083376bb654e12d9177414196691888f7977414
MrFermz <Mr.Fermz@hotmail.com>
2020-03-23 20:08:47 +0700

    add Desync func

--- api/method/post/reject_leaves.js
+++ api/method/post/reject_leaves.js
@@ -8,20 +8,19 @@ async function rejectLeaves(req, res, body) {
     
     if (token) {
         let data        = JSON.parse(body)
-        let sql         = `UPDATE leaves
-                           SET    status        = 2, 
-                                  dateReject    = '${data.dateReject}',
-                                  rejectReasons = '${data.reasons}'
-                           WHERE  leaveID       = ${data.leaveID}`
+        let sql     = `SELECT status FROM leaves WHERE leaveID = ${data.leaveID}`
         db.query(sql, async function (error, result) {
             if (error) {
                 result_failed['data']   = error
                 res.end(JSON.stringify(result_failed))
             } else {
-                let response    = await restoreDays(data.leavecountID, data.leaveType)
-                if (response.result == 'success') {
-                    res.end(JSON.stringify(result_success))
+                if (result[0].status == 0) {
+                    let update  = await updateStatus(data)
+                    if (update.result == 'success') {
+                        res.end(JSON.stringify(result_success))
+                    }
                 } else {
+                    result_failed['data']   = 'desync'
                     res.end(JSON.stringify(result_failed))
                 }
             }
@@ -30,9 +29,33 @@ async function rejectLeaves(req, res, body) {
 }
 
 
+function updateStatus(data) {
+    return new Promise(function (resolve, reject) {
+        let sql         = `UPDATE   leaves
+                           SET      status          = 2,
+                                    actionTimeStamp = '${data.dateReject}',
+                                    dateReject      = '${data.dateReject}',
+                                    rejectReasons   = '${data.reasons}'
+                           WHERE    leaveID         = ${data.leaveID}`
+        db.query(sql, async function (error, result) {
+            if (error) {
+                reject(error)
+            } else {
+                let response    = await restoreDays(data.leavecountID, data.leaveType)
+                if (response.result == 'success') {
+                    resolve(result_success)
+                } else {
+                    reject(result_failed)
+                }
+            }
+        })  
+    })
+}
+
+
 function restoreDays(leavecountID, leaveType) {
     return new Promise(function (resolve, reject) {
-        let sql     = `SELECT ${leaveType} FROM leavedays WHERE leavecountID = ${leavecountID}`
+        let sql     = `SELECT ${leaveType} FROM leavecount WHERE leavecountID = ${leavecountID}`
         db.query(sql, function (error, result) {
             if (error) reject(error)
             else {
