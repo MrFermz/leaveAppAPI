b2e457aff4f55e266416a7a57bfae6e50ed32ae7
MrFermz <Mr.Fermz@hotmail.com>
2020-03-24 17:31:24 +0700

    add restore days func

--- api/method/post/cancel.js
+++ api/method/post/cancel.js
@@ -15,7 +15,7 @@ async function cancel(req, res, body) {
                 res.end(JSON.stringify(result_failed))
             } else {
                 if (result[0].status == 0) {
-                    let update  = await updateStatus(data.id)
+                    let update  = await updateStatus(data)
                     if (update.result == 'success') {
                         res.end(JSON.stringify(result_success))
                     }
@@ -31,16 +31,43 @@ async function cancel(req, res, body) {
 }
 
 
-function updateStatus(id) {
+function updateStatus(data) {
     return new Promise(function (resolve, reject) {
+        console.log(data)
         let sql     = `UPDATE   leaves
-                        SET     status      = 3
-                        WHERE   leaveID     = ${id}`
-        db.query(sql, function (error, result) {
+                        SET     status          = 3,
+                                actionTimeStamp = '${data.actionTimeStamp}'
+                        WHERE   leaveID         = ${data.id}`
+        db.query(sql, async function (error, result) {
             if (error) {
                 reject(error)
             } else {
-                resolve(result_success)
+                let response    = await restoreDays(data.leavecountID, data.leaveType)
+                if (response.result == 'success') {
+                    resolve(result_success)
+                } else {
+                    reject(result_failed)
+                }
+            }
+        })
+    })
+}
+
+
+function restoreDays(leavecountID, leaveType) {
+    return new Promise(function (resolve, reject) {
+        let sql     = `SELECT ${leaveType} FROM leavecount WHERE leavecountID = ${leavecountID}`
+        db.query(sql, function (error, result) {
+            if (error) reject(error)
+            else {
+                let remain          = result[0]
+                let key             = Object.keys(remain)[0]
+                let value           = remain[key] - 1
+                let sql     = `UPDATE leavecount SET ${key} = ${value} WHERE leavecountID = ${leavecountID}`
+                db.query(sql, function (error, result) {
+                    if (error) reject(error)
+                    else resolve(result_success)
+                })
             }
         })
     })
